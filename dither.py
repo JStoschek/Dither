e7282b157c809fbbfa6178f2445618a024f0472d
JStoschek <jonathan.stoschek@gmail.com>
2026-03-18 15:19:54 -0700

    Convert image to grayscale

--- dither.py
+++ dither.py
@@ -5,3 +5,7 @@ img = Image.open(file_path)
 print(f"Opened: {file_path}")
 print(f"Size: {img.size[0]}x{img.size[1]}")
 print(f"Mode: {img.mode}")
+
+gray = img.convert("L")
+gray.save("cat_gray.png")
+print("Saved cat_gray.png")
